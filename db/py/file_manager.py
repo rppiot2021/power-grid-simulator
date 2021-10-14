4b504d241b8bde2af6e4e1f71a29e1abcccb75ba
marin-jovanovic <marin.jovanovic12@gmail.com>
2021-10-14 17:45:16 +0200

    :recycle: cleanup, fix multiple dir creation

--- db/py/file_manager.py
+++ db/py/file_manager.py
@@ -1,105 +1,70 @@
-"""
-backend module implementation for file logging
-"""
-
+import os
 from os.path import join
+from pathlib import Path
+
+import yaml
 
 from log_manager import LogManager
-from pathlib import Path
-import yaml
 
 
 class FileManager(LogManager):
-    # DEFAULT_FILE_PATH = "temp_file.db"
-    # DEFAULT_FILE_FOLDER = "file_log"
-    # DEFAULT_ARCHIVE_FOLDER = "file_archive"
-
-    ARE_CONSTANTS_LOADED = False
-
-    @staticmethod
-    def _init_constants():
-        FileManager.ARE_CONSTANTS_LOADED = True
-
-        with open("conf.yaml", "r") as stream:
-
-            # try:
-            t = yaml.safe_load(stream)["file_manager"]
-
-            FileManager.DEFAULT_FILE_PATH = t["default_file_path"]
-            FileManager.DEFAULT_FILE_FOLDER = t["default_file_folder"]
-            FileManager.DEFAULT_ARCHIVE_FOLDER = t["default_archive_folder"]
-
-            # except yaml.YAMLError as exc:
-            #     print(exc)
-
-    # def _constants_init(
-    #     self,
-    #     file_path=t["default_file_path"],
-    #     file_folder=t["default_file_folder"],
-    #     archive_folder=t["default_archive_folder"],
-    # ):
-    #     pass
 
     def __init__(
-        self,
-        file_path=None,
-        file_folder=None,
-        archive_folder=None
+            self,
+            file_path=None,
+            file_folder=None,
+            archive_folder=None
     ):
-
-        # if not FileManager.ARE_CONSTANTS_LOADED:
-        #     print("init")
-        #     FileManager._init_constants()
-
         with open("conf.yaml", "r") as stream:
-
-            # try:
             t = yaml.safe_load(stream)["file_manager"]
 
-            self.DEFAULT_FILE_PATH = file_path or t["default_file_path"]
-            self.DEFAULT_FILE_FOLDER =file_folder or  t["default_file_folder"]
-            self.DEFAULT_ARCHIVE_FOLDER =archive_folder or t["default_archive_folder"]
+            prefix = t["prefix"]
 
-            # print(self.DEFAULT_FILE_FOLDER)
-            # print(self.DEFAULT_FILE_PATH)
-            # print(self.DEFAULT_ARCHIVE_FOLDER)
+            if prefix == "None":
+                print("no prefix")
 
+                # for joining
+                prefix = ""
 
-            # self.DEFAULT_FILE_PATH = t["default_file_path"]
-            # self.DEFAULT_FILE_FOLDER = t["default_file_folder"]
-            # self.DEFAULT_ARCHIVE_FOLDER = t["default_archive_folder"]
+            else:
+                print("prefix exist", prefix)
 
+                if not os.path.exists(prefix):
+                    os.makedirs(prefix)
 
-        Path(self.DEFAULT_FILE_FOLDER).mkdir(parents=True, exist_ok=True)
-        Path(self.DEFAULT_ARCHIVE_FOLDER).mkdir(parents=True, exist_ok=True)
+            self.default_file_path = file_path or t["default_file_path"]
 
-        self.file_full_path = join(self.DEFAULT_FILE_FOLDER, self.DEFAULT_FILE_PATH)
+            self.default_file_folder = file_folder or join(prefix, t[
+                "default_file_folder"])
+            self.default_archive_folder = archive_folder or join(prefix, t[
+                "default_archive_folder"])
+
+        Path(self.default_file_folder).mkdir(parents=True, exist_ok=True)
+        Path(self.default_archive_folder).mkdir(parents=True, exist_ok=True)
+
+        self.file_full_path = join(self.default_file_folder,
+                                   self.default_file_path)
 
         self.file = open(self.file_full_path, "a")
+
         super().__init__(True)
 
     def _close(self):
-        self.check_connection()
-
         self.file.close()
-        self.is_opened = False
 
     def write(self, *data):
-        self.check_connection()
-
         self.file.write(str(data) + "\n")
 
 
 if __name__ == '__main__':
-
-    with FileManager("a", "b", "c") as file:
-        file.write("t1", "t2")
-        print(file.DEFAULT_FILE_FOLDER)
-        print(file.DEFAULT_FILE_PATH)
-        print(file.DEFAULT_ARCHIVE_FOLDER)
+    # with FileManager("a", "b", "c") as file:
+    #     file.write("t1", "t2")
+    #     print(file.default_file_folder)
+    #     print(file.default_file_path)
+    #     print(file.default_archive_folder)
 
     with FileManager() as file:
         file.write("t1", "t2")
-        print(file.DEFAULT_FILE_FOLDER)
-        print(file.DEFAULT_FILE_PATH)
-        print(file.DEFAULT_ARCHIVE_FOLDER)
+        print(file.default_file_folder)
+        print(file.default_file_path)
+        print(file.default_archive_folder)
