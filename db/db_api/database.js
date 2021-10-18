20c10e0251259e88d7fb7a21fbaae6fe94b94afe
marin-jovanovic <marin.jovanovic12@gmail.com>
2021-10-18 11:15:08 +0200

    :recycle: cleanup

--- db/db_api/database.js
+++ db/db_api/database.js
@@ -1,36 +1,33 @@
 let sqlite3 = require("sqlite3").verbose();
-
-// database file name
-const DB_SOURCE = "../db_log/sql_db.db_api";
-
 const fs = require('fs');
 const yaml = require('js-yaml');
+const path = require('path');
 
-try {
-    // let fileContents = fs.readFileSync('./data-multi.yaml', 'utf8');
-    let fileContents = fs.readFileSync('../conf.yaml', 'utf8');
+function getDbPath() {
+  let fileContents = fs.readFileSync('../conf.yaml', 'utf8');
+  let data = yaml.loadAll(fileContents);
+  let dbConfig = data[0]["database_manager"];
 
-    let data = yaml.loadAll(fileContents);
+  let fullPath = dbConfig["prefix"] === "None" ? "" : dbConfig["prefix"];
 
-    let db_config = data[0]["database_manager"]
+  fullPath += path.join(
+    dbConfig["default_database_folder"],
+    dbConfig["default_database_filename"]
+  );
 
-    console.log(db_config)
-    // console.log(data[0]["database_manager"])
+  console.log(fullPath);
 
-    console.log("data------------------------------------")
-    console.log(data);
-} catch (e) {
-    console.log(e);
+  return fullPath;
 }
-//
-// let db = new sqlite3.Database(DB_SOURCE, (err) => {
-//   if (err) {
-//     console.log("Error connecting to SQlite database.");
-//     console.error(err.message);
-//     throw err;
-//   } else {
-//     console.log("Connected to the SQlite database.");
-//   }
-// });
-//
-// module.exports = db;
+
+let db = new sqlite3.Database(getDbPath(), (err) => {
+  if (err) {
+    console.log("Error connecting to SQlite database.");
+    console.error(err.message);
+    throw err;
+  } else {
+    console.log("Connected to the SQlite database.");
+  }
+});
+
+module.exports = db;
